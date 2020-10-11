2c794f9464aa18e6abc3e6b5870bd0f52b51da22
koookee <55768904+koookee@users.noreply.github.com>
2020-10-11 15:53:12 -0400

    In the process of adding nav mesh agents

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyScript : MonoBehaviour
 {
+    NavMeshAgent agent;
+    PlayerController player;
     private int health = 5;
+    private float range = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         CheckHealth();
+        MoveToPlayer();
     }
     public void TakeDamage(int damageTook)
     {
@@ -23,5 +29,19 @@ public class EnemyScript : MonoBehaviour
     private void CheckHealth()
     {
         if (health <= 0) Destroy(gameObject);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        //Not able to call range for some reason
+        Gizmos.DrawWireSphere(transform.position, 7f);
+    }
+    private void MoveToPlayer()
+    {
+        float distance = Vector3.Distance(player.transform.position,transform.position);
+        if(distance <= range)
+        {
+            agent.SetDestination(player.transform.position);
+        }
     }
 }
