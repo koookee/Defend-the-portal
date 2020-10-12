e129c129ca47bec7ed1046dfc9b21851e8d6a1a5
koookee <55768904+koookee@users.noreply.github.com>
2020-10-12 04:13:55 -0400

    Finished implementing the nevmesh agent

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -8,7 +8,9 @@ public class EnemyScript : MonoBehaviour
     NavMeshAgent agent;
     PlayerController player;
     private int health = 5;
-    private float range = 2f;
+    private float detectionRange = 7f;
+    private float attackRange = 1.5f;
+    private bool isAlive = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,20 +30,39 @@ public class EnemyScript : MonoBehaviour
     }
     private void CheckHealth()
     {
-        if (health <= 0) Destroy(gameObject);
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            isAlive = false;
+        }
+    }
+    
+    private void MoveToPlayer()
+    {
+        float distance = Vector3.Distance(player.transform.position,transform.position);
+        if (distance <= detectionRange && distance >= attackRange && isAlive)
+        {
+            //Moves enemy to player
+            //Reason as to why I added isAlive is so that it doesn't execute after the enemy is dead
+            //which causes a bug
+            agent.SetDestination(player.transform.position);
+        }
+        else
+        {
+            //If enemy is in combat range, they look at the player
+            LookAtPlayer();
+        }
+    }
+    private void LookAtPlayer()
+    {
+        //Enemy looks at player when they are in combat range
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x,0,direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        //Not able to call range for some reason
-        Gizmos.DrawWireSphere(transform.position, 7f);
-    }
-    private void MoveToPlayer()
-    {
-        float distance = Vector3.Distance(player.transform.position,transform.position);
-        if(distance <= range)
-        {
-            agent.SetDestination(player.transform.position);
-        }
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
     }
 }
