41fa67bf66c2edc50a1c3d197975de65ff04056a
koookee <55768904+koookee@users.noreply.github.com>
2020-10-14 04:29:10 -0400

    Enemy can now damage portal. Fixed bug that set the agent destination after game object was destroyed

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -6,26 +6,30 @@ using UnityEngine.AI;
 
 public class EnemyScript : MonoBehaviour
 {
-    NavMeshAgent agent;
-    PlayerController player;
+    private NavMeshAgent agent;
+    private PlayerController player;
+    private PortalScript portal;
     private int health = 5;
     private float detectionRange = 7f;
-    private float attackRange = 1.5f;
+    private float attackRange; //attack range is dependant on the stopping distance
     private bool isAlive = true;
     private float attackSpeed = 1f; //1 attacks per second
     private float attackCooldown = 0f; //Updates with Time.time
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.Find("Player").GetComponent<PlayerController>();
+        portal = GameObject.Find("Portal").GetComponent<PortalScript>();
+        attackRange = agent.stoppingDistance;
     }
 
     // Update is called once per frame
     void Update()
     {
         CheckHealth();
-        MoveToPlayer();
+        MoveToTarget();
     }
     public void TakeDamage(int damageTook)
     {
@@ -36,59 +40,65 @@ public class EnemyScript : MonoBehaviour
     {
         if (health <= 0)
         {
-            Destroy(gameObject);
             isAlive = false;
+            Destroy(gameObject);
         }
     }
-    
-    private void MoveToPlayer()
+
+    private void MoveToTarget()
     {
-        float distance = Vector3.Distance(player.transform.position,transform.position);
-        if (distance <= detectionRange && distance >= attackRange && isAlive)
-        {
-            //Moves enemy to player
-            //Reason as to why I added isAlive is so that it doesn't execute after the enemy is dead
-            //which causes a bug
-            agent.SetDestination(player.transform.position);
-        }
-        else if (distance <= attackRange)
-        {
-            //If enemy is in combat range, they look at the player
-            LookAtPlayer();
-            AttackPlayer(attackSpeed);
-        }
-        else
-        {
-            //If player is not in range, enemy will target portal
-            MoveToPortal();
-        }
-    }
-    private void MoveToPortal()
-    {
+        //Update method gets called after enemy is destroyed
+        //The if condition prevents the method from executing if the enemy is destroyed
         if (isAlive)
         {
-            //Moves enemy to portal
-            agent.SetDestination(GameObject.Find("Portal").transform.position);
+            float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            float distanceToPortal = Vector3.Distance(portal.transform.position, transform.position);
+
+            if (distanceToPlayer <= detectionRange && distanceToPlayer >= attackRange)
+            {
+                //Moves enemy to player
+                //Reason as to why I added isAlive is so that it doesn't execute after the enemy is dead
+                //which causes a bug
+                agent.SetDestination(player.transform.position);
+            }
+            else if (distanceToPlayer <= attackRange)
+            {
+                //If enemy is in combat range, they look at the player
+                LookAtPlayer();
+                Attack(attackSpeed,"Player");
+            }
+            else
+            {
+                //If player is not in range, enemy will target portal
+                agent.SetDestination(portal.transform.position);
+                if (distanceToPortal <= attackRange)
+                {
+                    Debug.Log("Im in");
+                    Attack(attackSpeed,"Portal");
+                }
+            }
         }
     }
     private void LookAtPlayer()
     {
         //Enemy looks at player when they are in combat range
         Vector3 direction = (player.transform.position - transform.position).normalized;
-        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x,0,direction.z));
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
-    private void AttackPlayer(float attackSpeed)
+    private void Attack(float attackSpeed, string objectToAttack)
     {
         //Play attack animation
         //Wait until animation is over to do damage
         if (Time.time > attackCooldown)
         {
-            player.health--;
+            if (objectToAttack == "Player") player.health--;
+            else portal.health--;
             attackCooldown = Time.time + 1 / attackSpeed;
             Debug.Log("Player health: " + player.health);
+            Debug.Log("Portal health: " + portal.health);
         }
-        
+
     }
     private void OnDrawGizmosSelected()
     {
--- Assets/Scripts/PortalScript.cs
+++ Assets/Scripts/PortalScript.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class Enemy1 : MonoBehaviour
+public class PortalScript : MonoBehaviour
 {
+    public int health = 100;
+
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
     }
 }
