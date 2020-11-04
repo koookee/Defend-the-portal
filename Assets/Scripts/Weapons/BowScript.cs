59b92bad60ba0566d6f9ed3a4a0dc793d16e737a
koookee <55768904+koookee@users.noreply.github.com>
2020-11-04 03:47:39 -0500

    Experimenting with a bow and arrow

---
+++ Assets/Scripts/Weapons/BowScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowScript : MonoBehaviour
+{
+    public GameObject arrow;
+    public GameObject arrowPos; //Empty game object that specifies the arrow's position
+    public Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject arr = Instantiate(arrow, arrowPos.transform.position, mainCamera.transform.rotation);
+            arr.GetComponent<Rigidbody>().AddForce(arr.transform.forward * 10, ForceMode.Impulse);
+        }
+    }
+}
