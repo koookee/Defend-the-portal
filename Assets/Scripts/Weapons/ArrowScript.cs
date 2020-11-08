ff19c7bcc5d8c2df02ac4de4ba8ede6598dccc02
koookee <55768904+koookee@users.noreply.github.com>
2020-11-08 02:57:35 -0500

    Added a fire rate to the bow. Added damage that's dependant on the force multiplier. Arrows are destroyed when in contact with another collider.

---
+++ Assets/Scripts/Weapons/ArrowScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    public int damage; //Gets assigned in the BowScript
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<EnemyScript>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Weapons/BowScript.cs
+++ Assets/Scripts/Weapons/BowScript.cs
@@ -8,8 +8,15 @@ public class BowScript : MonoBehaviour
     public GameObject Arrow; //Arrow prefab
     public PlayerController Player;
     public Camera mainCamera;
+
     private int arrowForce = 15;
-    private float timeStart;
+    private float timeStart; 
+
+    private int fireRate = 1; //Number of arrows in a second
+    private float timeToWait = 0f;
+
+    private int arrowDamage = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,20 +30,26 @@ public class BowScript : MonoBehaviour
     }
     void Shoot()
     {
-        if (Input.GetButtonDown("Fire1") && Player.inventorySlotNum == 2)
+        if (Time.time > timeToWait)
         {
-            timeStart = Time.time;
-        }
-        if (Input.GetButtonUp("Fire1"))
-        {
-            float timeDown = Time.time - timeStart; //Time spent holding down the fire1 button
-            float forceMultiplier = timeDown * 2;
-            if (forceMultiplier >= 4) forceMultiplier = 4; //Max limit for the force multiplier
-            if (forceMultiplier <= 1) forceMultiplier = 1; //Min limit for the force multiplier
-            Debug.Log(forceMultiplier);
-            Vector3 spawnPos = transform.position + transform.forward + transform.up * 0.5f;
-            GameObject arrow = Instantiate(Arrow, spawnPos, mainCamera.transform.rotation);
-            arrow.GetComponent<Rigidbody>().AddForce(arrow.transform.forward * arrowForce * forceMultiplier, ForceMode.Impulse);
+            if (Input.GetButtonDown("Fire1") && Player.inventorySlotNum == 2)
+            {
+                timeStart = Time.time;
+            }
+            if (Input.GetButtonUp("Fire1") && Player.inventorySlotNum == 2)
+            {
+                float timeDown = Time.time - timeStart; //Time spent holding down the fire1 button
+                float forceMultiplier = timeDown * 2;
+                if (forceMultiplier >= 4) forceMultiplier = 4; //Max limit for the force multiplier
+                if (forceMultiplier <= 1) forceMultiplier = 1; //Min limit for the force multiplier
+                arrowDamage = (int)forceMultiplier;
+                Debug.Log(forceMultiplier);
+                Vector3 spawnPos = transform.position + transform.forward + transform.up * 0.5f;
+                GameObject arrow = Instantiate(Arrow, spawnPos, mainCamera.transform.rotation);
+                arrow.GetComponent<Rigidbody>().AddForce(arrow.transform.forward * arrowForce * forceMultiplier, ForceMode.Impulse);
+                arrow.GetComponent<ArrowScript>().damage = arrowDamage;
+                timeToWait = Time.time + 1/fireRate;
+            }
         }
     }
 }
